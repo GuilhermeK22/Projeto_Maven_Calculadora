7089cf4872602a9ff4c2120dbfcb32737e3ef7cd
Guilherme <guilhermekappaun@gmail.com>
2024-01-24 05:59:18 -0300

    V1

--- src/main/java/Calculadora.java
+++ src/main/java/Calculadora.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Calculadora {
+public class Calculadora {
+    private double a, b;
+
+    public Calculadora(double a, double b) {
+        this.a = a;
+        this.b = b;
+    }
+    public double soma() {
+        return this.a + this.b;
+    }
 }
--- src/test/java/CalculadoraTest.java
+++ src/test/java/CalculadoraTest.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class CalculadoraTest {
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class CalculadoraTest {
+    @Test
+    public void testSoma() {
+        Calculadora c = new Calculadora(2, 3);
+        assertEquals(5.0, c.soma(), 0.001);
+    }
 }
